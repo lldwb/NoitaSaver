df898f18d08b1419b6bb38c5e0a99daf3d671e72
lldwb <3247187440@qq.com>
2023-04-22 23:51:16 +0800

    安然的尾巴

--- src/test/java/top/lldwb/noitaSaver/service/copy/ArchiveFolderCopyImplTest.java
+++ src/test/java/top/lldwb/noitaSaver/service/copy/ArchiveFolderCopyImplTest.java
@@ -9,7 +9,7 @@ public class ArchiveFolderCopyImplTest {
 
     @Test
     public void tset() {
-        String writePath="C:\\Users\\lldwb\\AppData\\LocalLow\\Nolla_Games_Noita\\save00";
+        String writePath="C:\\Users\\lldwb\\Desktop\\Noita\\2023-04-22_18-23-33";
 
         String readPath="C:\\Users\\lldwb\\Desktop\\Noita\\";
         FolderCopy folderCopy = new ArchiveFolderCopyImpl();
--- src/test/java/top/lldwb/noitaSaver/service/copy/UnArchiveFolderCopyImplTest.java
+++ src/test/java/top/lldwb/noitaSaver/service/copy/UnArchiveFolderCopyImplTest.java
@@ -6,7 +6,7 @@ public class UnArchiveFolderCopyImplTest {
     @Test
     public void test(){
 
-        String writePath="C:\\Users\\lldwb\\Desktop\\Noita\\4";
+        String writePath="C:\\Users\\lldwb\\Desktop\\Noita\\2023-04-22_18-23-33";
 
         String readPath="C:\\Users\\lldwb\\AppData\\LocalLow\\Nolla_Games_Noita\\save00";
         FolderCopy folderCopy = new UnArchiveFolderCopyImpl();
