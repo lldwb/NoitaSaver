2d97a7a07028adbf0af173ba2b50c3b5d2d20516
lldwb <3247187440@qq.com>
2023-04-23 19:50:11 +0800

    安然的尾巴

---
+++ src/main/java/top/lldwb/noitaSaver/action/ArchiveFolderCopy.java
@@ -0,0 +1,33 @@
+package top.lldwb.noitaSaver.action;
+
+import top.lldwb.noitaSaver.service.copy.ArchiveFolderCopyImpl;
+import top.lldwb.noitaSaver.service.copy.FolderCopy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 存档API
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+@WebServlet("/archiveFolderCopy")
+public class ArchiveFolderCopy extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+
+        // 源文件夹地址
+        String writePath = req.getParameter("writePath");
+        // 目标文件夹地址
+        String readPath = req.getParameter("readPath");
+
+        FolderCopy folderCopy = FolderCopy.getFolderCopyFactory("存档");
+        folderCopy.copy(writePath, readPath);
+    }
+}
