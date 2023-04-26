30d5c58c72f2795e70fba50288e77397c998f239
lldwb <3247187440@qq.com>
2023-04-26 23:25:31 +0800

    Socket测试

--- NoitaSaverClient/src/test/java/Test.java
+++ NoitaSaverClient/src/test/java/Test.java
@@ -20,19 +20,25 @@ public class Test {
             // 输出流对象用来向服务器发送数据
             OutputStream os = s.getOutputStream();
 
-            // 创建一个Writer对象bw，这个对象用于向Socket的输出流os中写入字符数据。数据会被先写入到BufferedWriter中，再通过OutputStreamWriter将数据转化为字节流
+            // 创建一个Writer对象bw，这个对象用于向Socket的输出流os中写入字符数据
+            // 数据会被先写入到BufferedWriter中，再通过OutputStreamWriter将数据转化为字节流
             Writer bw = new BufferedWriter(new OutputStreamWriter(os));
+//            Writer bw = new OutputStreamWriter(os);
             //向服务器端发送一条消息
+            // 在缓冲输出流BufferedWriter bw 中写入字符串
             bw.write("测试客户端和服务器通信，服务器接收到消息返回到客户端\n");
             // 通过flush()方法刷新缓存，确保数据被立即发送
             bw.flush();
 
             //读取服务器返回的消息
+            // 创建一个缓冲输入流，用于读取来自服务器的输入流(InputStream)中的数据，并将其转换为字符串
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            // br.readLine读取一行数据
             String mess = br.readLine();
 
             System.out.println("服务器："+mess);
-            s.close();
+            // 关闭连接
+//            s.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
