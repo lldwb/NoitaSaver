e2cef53bcad95904f5fdfd35137ca012f1e4bc4e
lldwb <3247187440@qq.com>
2023-05-04 09:56:05 +0800

    完善DbUtil

---
+++ DbUtil/src/main/java/top/lldwb/noitaSaver/DbUtil/DbUtils.java
@@ -0,0 +1,23 @@
+package top.lldwb.noitaSaver.DbUtil;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 谢世杰
+ * @version 1.0
+ */
+public final class DbUtils {
+    public static void close(Connection conn) throws SQLException {
+        if (conn != null){
+            conn.close();
+        }
+    }
+    public static void close(ResultSet rs) throws SQLException {
+        if (rs != null){
+            rs.close();
+        }
+    }
+}
