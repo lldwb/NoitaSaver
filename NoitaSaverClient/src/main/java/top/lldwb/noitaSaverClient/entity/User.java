385e4389d2be12dbba9b8bd5ec491dc68e9bc507
lldwb <3247187440@qq.com>
2023-05-05 10:09:11 +0800

    用户实体类
    用户实体类

---
+++ NoitaSaverClient/src/main/java/top/lldwb/noitaSaverClient/entity/User.java
@@ -0,0 +1,25 @@
+package top.lldwb.noitaSaverClient.entity;
+
+import lombok.Data;
+
+/**
+ * 用户实体类
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+@Data
+public class User {
+    public int id;
+    /**
+     * 用户名
+     */
+    public String name;
+    /**
+     * 邮箱认证状态
+     */
+    public boolean emailAuthentication;
+    /**
+     * 访问秘钥，(id+时间戳+随机函数)使用MD5加密
+     */
+    public String accesskey;
+}
