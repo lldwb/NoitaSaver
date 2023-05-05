e45a14c8554a6040fb75a05d00a831042bed8fe8
lldwb <3247187440@qq.com>
2023-05-05 09:45:07 +0800

    更新 TimeUtil.java
    时间工具类完善

--- NoitaSaverClient/src/main/java/top/lldwb/noitaSaverClient/utils/TimeUtil.java
+++ NoitaSaverClient/src/main/java/top/lldwb/noitaSaverClient/utils/TimeUtil.java
@@ -6,7 +6,8 @@ import java.time.format.DateTimeFormatter;
 public class TimeUtil {
     public static String currentTime(){
         // 自定义时间格式
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
         // 返回根据自定义时间格式生成的字符串
         return dateTimeFormatter.format(LocalDateTime.now());
     }
