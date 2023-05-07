bb16eb1052d73a5e7e6450ff0c6ca3e5b0da0809
lldwb <3247187440@qq.com>
2023-05-07 20:40:01 +0800

    默认

--- NoitaSaverServer/src/main/java/top/lldwb/noitaSaverServer/servlet/Dispatcher/DispatcherServlet.java
+++ NoitaSaverServer/src/main/java/top/lldwb/noitaSaverServer/servlet/Dispatcher/DispatcherServlet.java
@@ -31,6 +31,10 @@ public class DispatcherServlet extends HttpServlet {
     /**
      * 加载配置文件
      *
+     * GenericServlet.init()的源码中文翻译
+     *这是一个便利的方法，可以被重写以避免调用super.init(config)。
+     * 不需要重写init(ServletConfig)，只需要重写这个方法，GenericServlet.init(ServletConfig config)将会调用它。ServletConfig对象仍然可以通过getServletConfig方法获取。
+     *
      * @throws ServletException
      */
     @Override
@@ -46,6 +50,29 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
+    /**
+     * 重写 HttpServlet.service(HttpServletRequest req, HttpServletResponse resp)，
+     * HttpServlet.service(HttpServletRequest req, HttpServletResponse resp) 又是重写
+     * GenericServlet.service(ServletRequest req, ServletResponse res)。
+     * 这是一个便利的方法，可以被重写以避免调用super.init(config)。
+     *
+     * GenericServlet.service(ServletRequest req, ServletResponse res)的源码中文翻译
+     * 不需要重写init(ServletConfig)，只需要重写这个方法，GenericServlet.init(ServletConfig config)将会调用它。ServletConfig对象仍然可以通过getServletConfig方法获取。
+     *
+     * HttpServlet.service(ServletRequest req, ServletResponse res)的源码中文翻译
+     * 从公共服务方法接收标准的HTTP请求，并将它们分派到该类中定义的doMethod方法。这个方法是javax.servlet.Servlet.service方法的HTTP特定版本。不需要重写此方法。
+     *
+     * @param req   the {@link HttpServletRequest} object that
+     *                  contains the request the client made of
+     *                  the servlet
+     *
+     * @param resp  the {@link HttpServletResponse} object that
+     *                  contains the response the servlet returns
+     *                  to the client
+     *
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
---
+++ NoitaSaverServer/src/main/java/top/lldwb/noitaSaverServer/servlet/WebController.java
@@ -0,0 +1,16 @@
+package top.lldwb.noitaSaverServer.servlet;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author 安然的尾巴
+ * @version 1.0
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WebController {
+    String value();
+}
